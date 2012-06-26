b4ce4a93c601ef20eb6761239c5162b91d99d2e5
Thomas Stachl <thomas@stachl.me>
2012-06-26 12:18:07 +0100

    Added sessions store for desk.com site so it is changeable at runtime

--- lib/omniauth/strategies/desk.rb
+++ lib/omniauth/strategies/desk.rb
@@ -6,6 +6,8 @@ module OmniAuth
     # An omniauth 1.0 strategy for Desk.com authorization
     class Desk < OmniAuth::Strategies::OAuth
       option :name, 'desk'
+      option :site, nil
+      option :site_param, 'desk_site'
       option :client_options, {
         :authorize_path       => '/oauth/authorize',
         :request_token_path   => '/oauth/request_token',
@@ -35,7 +37,7 @@ module OmniAuth
 
       # Return info gathered from the verify_credentials API call 
       def raw_info
-        @raw_info ||= MultiJson.decode(access_token.get('/api/v1/account/verify_credentials.json').body)
+        @raw_info ||= MultiJson.decode(access_token.get('/api/v1/account/verify_credentials.json').body) if access_token
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
       end
@@ -45,13 +47,32 @@ module OmniAuth
         @user_info ||= raw_info.nil? ? {} : raw_info['user']
       end
       
+      def identifier
+        session[:site] = options.client_options.site = options.site || validate_site(request.params[options.site_param.to_s])
+        session[:site] = options.client_options.site = nil if options.client_options.site == ''
+        options.client_options.site
+      end
+      
+      def validate_site(site)
+        if site and site != ''
+          "https://#{site}.desk.com"
+        end
+      end
+      
+      def get_identifier
+        f = OmniAuth::Form.new :title => 'Desk.com Authorization'
+        f.text_field 'Desk.com Site', options.site_param.to_s
+        f.html '<p><strong>Hint:</strong> https://YOURSITE.desk.com'
+        f.button 'Login'
+        f.to_response
+      end
+      
       def request_phase
-        options[:client_options][:site] = options[:site] if options[:site]
-        super
+        identifier ? super : get_identifier
       end
 
       def callback_phase
-        options[:client_options][:site] = options[:site] if options[:site]
+        options.client_options.site = session[:site] if session[:site]
         super
       end
     end
--- spec/omniauth/strategies/desk_spec.rb
+++ spec/omniauth/strategies/desk_spec.rb
@@ -1,25 +1,70 @@
 require 'spec_helper'
+require 'omniauth-desk'
 
-describe OmniAuth::Strategies::Desk do
-  subject do
-    OmniAuth::Strategies::Desk.new({})
+describe OmniAuth::Strategies::Desk, :type => :strategy do
+  def app
+    strat = OmniAuth::Strategies::Desk
+    Rack::Builder.new {
+      use Rack::Session::Cookie
+      use strat
+      run lambda {|env| [404, {'Content-Type' => 'text/plain'}, [nil || env.key?('omniauth.auth').to_s]] }
+    }.to_app
+  end
+  
+  describe '/auth/desk without a site URL' do
+    before do
+      get '/auth/desk'
+    end
+    
+    it 'should respond with OK' do
+      last_response.should be_ok
+    end
+    
+    it 'should respond with HTML' do
+      last_response.content_type.should == 'text/html'
+    end
+
+    it 'should render an identifier URL input' do
+      last_response.body.should =~ %r{<input[^>]*desk_site}
+    end
+  end
+  
+  describe 'followed by /auth/desk/callback' do
+    context 'successful' do
+      it 'should set provider to desk'
+    end
+    
+    context 'unsuccessful' do
+      before do
+        get '/auth/desk/callback'
+      end
+      
+      it 'should be redirected to failure' do
+        last_response.should be_redirect
+        last_response.headers['Location'].should =~ %r{failure}
+      end
+    end
   end
 
-  context "client options" do
-    it 'should have correct name' do
-      subject.options.name.should eq("desk")
-    end
-
-    it 'should have correct authorize path' do
-      subject.options.client_options.authorize_path.should eq('/oauth/authorize')
-    end
-
-    it 'should have correct request token path' do
-      subject.options.client_options.request_token_path.should eq('/oauth/request_token')
-    end
-
-    it 'should have correct access token path' do
-      subject.options.client_options.access_token_path.should eq('/oauth/access_token')
-    end
-  end
+  # context "client options" do
+  #   it 'should have correct name' do
+  #     subject.options.name.should eq("desk")
+  #   end
+  # 
+  #   it 'should have correct authorize path' do
+  #     subject.options.client_options.authorize_path.should eq('/oauth/authorize')
+  #   end
+  # 
+  #   it 'should have correct request token path' do
+  #     subject.options.client_options.request_token_path.should eq('/oauth/request_token')
+  #   end
+  # 
+  #   it 'should have correct access token path' do
+  #     subject.options.client_options.access_token_path.should eq('/oauth/access_token')
+  #   end
+  #   
+  #   it 'should have an empty user_info' do
+  #     subject.user_info.should eq({})
+  #   end
+  # end
 end
